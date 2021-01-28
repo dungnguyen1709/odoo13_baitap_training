77fc335e6b6bb65cd81a1c4bb87cefc7d8d29866
dungnguyen1709 <lethanhnghi135@yahoo.com>
2021-01-28 15:55:47 +0700

    setting display list and total debts

--- models/res_partner.py
+++ models/res_partner.py
@@ -3,18 +3,9 @@ from odoo import models, fields, api, _
 
 class ResPartner(models.Model):
     _inherit = 'res.partner'
-    _name = 'partner.model'
 
     owe_limit = fields.Integer(string='Owe Limit', default=5000, readonly=True)
 
     owe_calculation = fields.Selection(
         selection=[('discount', 'Discount'), ('no discount', 'No Discount'), ('send by email', 'Send By Email')],
         string='Type', default='discount')
-
-    debts = fields.One2many(string='Debts')
-
-
-class AccountMove(models.Model):
-    _name = 'account.model'
-
-    account_id = fields.Many2one('partner.model', string='Account ID')
