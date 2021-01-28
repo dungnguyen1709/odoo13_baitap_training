26b9eb4cd8650f56485de4710242bc0ace72b1a8
dungnguyen1709 <lethanhnghi135@yahoo.com>
2021-01-28 09:43:20 +0700

    them thong tin config edit view form

--- models/res_partner.py
+++ models/res_partner.py
@@ -1,24 +1,13 @@
-from odoo import models, fields, api, _
+from odoo import fields, models, _
 
 
 class ResPartner(models.Model):
-    _inherit = 'res.partner'
+    _inherit = "sale.order.line"
 
-    debt_limit = fields.Integer(string='Debt Limit', default=50000)
-    debt_calculation = fields.Selection(
-        selection_add=[('discount', 'Discount'), ('no_discount', 'No Discount'), ('send_by_email', 'Send By Email')],
-        string='Type')
+    owe_limit = fields.Integer(string='Owe Limit', default=5000)
 
-    @api.depends('price')
-    def yes_discount(self):
-        for rec in self:
-            if rec.price * 0.9:
-                rec.discount = '10%'
-            elif rec.price * 0.7:
-                rec.discount = '30%'
-            else:
-                rec.discount = rec.price * 0.5
-                rec.discount = '50%'
 
-    discount = fields.Selection(selection=[('10%', 'Discount 10%'), ('30%', 'Discount 30%'), ('50%', 'Discount 50%')],
-                                compute='yes_discount')
+class ResPartner(models.Model):
+    _inherit = "sale.order"
+
+    owe_limit = fields.Integer(string='Owe Limit', related="order_line.owe_limit")
