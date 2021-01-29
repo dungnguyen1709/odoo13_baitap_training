8605aec153345d4fe2c4ab67afeb88a7a55cfd8a
dungnguyen1709 <lethanhnghi135@yahoo.com>
2021-01-29 16:44:55 +0700

    commit

--- models/sale_order.py
+++ models/sale_order.py
@@ -12,6 +12,28 @@ class SaleOrder(models.Model):
     def get_remaining_debt(self):
         for rec in self:
             rec.remaining_debt = rec.partner_id.get_remaining_debt()
+            print(rec.remaining_debt)
+
+    def action_confirm(self):
+        for rec in self:
+            cart_total = 0
+            for product in rec.order_line:
+                cart_total += product.price_subtotal
+
+            if cart_total > rec.partner_id.get_remaining_debt():
+                print(cart_total)
+                print(rec.partner_id.get_remaining_debt())
+                return {
+                    'name': _('Sale'),
+                    'view_type': 'form',
+                    'res_model': 'sale.order',
+                    'view_id': False,
+                    'view_mode': 'tree,form',
+                    'type': 'ir.actions.act_window',
+                    'type': 'ir.actions.act_window',
+                }
+            else:
+                super(SaleOrder, self).action_confirm()
 
     # @api.onchange('order_line')
     # def check_can_buy(self):
